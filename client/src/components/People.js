af81d28f43ddafcfe87cf0542557a07c0e1b3b22
Feyzan Aydemir <feyzan.aydemir@gmail.com>
2022-02-20 15:44:01 +0300

    Add people and settings components

---
+++ client/src/components/People.js
@@ -0,0 +1,97 @@
+import React, { useEffect, useState, useContext } from 'react';
+import { useParams, useHistory } from 'react-router';
+import { Context } from '../context/Context';
+import { Link } from 'react-router-dom';
+import axios from 'axios';
+import { LinearProgress } from '@mui/material';
+import defaultImage from '../assets/images/default-image.png';
+import '../assets/styles/People.css';
+
+function People() {
+  const { user } = useContext(Context);
+  const [people, setPeople] = useState([]);
+  const [isFetching, setIsFetching] = useState(false);
+  const paramUsername = useParams().username;
+  const history = useHistory();
+
+  useEffect(() => {
+    const getPeople = async () => {
+      try {
+        setIsFetching(true);
+        const peopleList = await axios.get(`/api/users/${user._id}/following`);
+        setIsFetching(false);
+
+        setPeople(peopleList.data);
+        sessionStorage.setItem(
+          'gipost-people',
+          JSON.stringify(peopleList.data)
+        );
+      } catch (err) {
+        console.log(err);
+      }
+    };
+
+    if (!paramUsername || paramUsername !== user.username) {
+      history.push('/oops');
+    } else {
+      if (sessionStorage.getItem('gipost-people')) {
+        const storedPeople = JSON.parse(
+          sessionStorage.getItem('gipost-people')
+        );
+        let count = 0;
+
+        for (let i = 0; i < user.following.length; i++) {
+          if (user.following[i] === storedPeople[i]?._id) {
+            count++;
+          } else {
+            break;
+          }
+        }
+
+        if (count === user.following.length) {
+          setPeople(storedPeople);
+        } else {
+          getPeople();
+        }
+      } else {
+        getPeople();
+      }
+    }
+
+    return () => setPeople([]);
+  }, [user]);
+
+  return (
+    <>
+      {isFetching ? (
+        <div className="people">
+          <LinearProgress color="secondary" />
+        </div>
+      ) : (
+        <div className="people">
+          <div className="people-container">
+            {people.map((person) =>
+              person ? (
+                <Link
+                  to={`/u/${person.username}`}
+                  style={{ textDecoration: 'none' }}
+                  key={person._id}
+                >
+                  <div>
+                    <img
+                      alt="User profile image."
+                      src={person.image ? person.image : defaultImage}
+                    />
+                    <span>{person.username}</span>
+                  </div>
+                </Link>
+              ) : null
+            )}
+          </div>
+        </div>
+      )}
+    </>
+  );
+}
+
+export default People;
---
+++ client/src/components/Settings.js
@@ -0,0 +1,191 @@
+import React, { useContext, useRef, useState, useEffect } from 'react';
+import { useParams, useHistory } from 'react-router';
+import { Link } from 'react-router-dom';
+import { Context } from '../context/Context';
+import { updateAccount, deleteAccount } from '../apiCalls/userCalls';
+import axios from 'axios';
+import settingsGif from '../assets/images/settings.gif';
+import '../assets/styles/Settings.css';
+
+function Settings() {
+  const { user, dispatch } = useContext(Context);
+  const [showConfirmation, setShowConfirmation] = useState(false);
+  const [validationErrors, setValidationErrors] = useState([]);
+  const [isUpdateSuccessful, setIsUpdateSuccessful] = useState(false);
+  const newUsername = useRef();
+  const newEmail = useRef();
+  const currentPassword = useRef();
+  const newPassword = useRef();
+  const username = useParams().username;
+  const history = useHistory();
+
+  useEffect(() => {
+    const getUser = async () => {
+      try {
+        await axios.get(`/api/users?username=${username}`);
+      } catch (err) {
+        history.push('/oops');
+      }
+    };
+
+    // Check if user is on their Settings page and not someone elses
+    if (!username || username !== user.username) {
+      history.push('/oops');
+    } else {
+      getUser();
+    }
+  }, []);
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+
+    // Set temporary keys for validation and authentication
+    user.newUsername = newUsername.current.value;
+    user.newEmail = newEmail.current.value;
+    user.currentPassword = currentPassword.current.value;
+    user.newPassword = newPassword.current.value;
+
+    const errors = await updateAccount(user, dispatch);
+
+    if (errors) {
+      setValidationErrors(errors);
+    } else {
+      setValidationErrors([]);
+      setIsUpdateSuccessful(true);
+
+      currentPassword.current.value = '';
+      newPassword.current.value = '';
+
+      history.push(`/u/${newUsername.current.value}/settings`);
+    }
+  };
+
+  const cancelChanges = () => {
+    newUsername.current.value = user.username;
+    newEmail.current.value = user.email;
+    currentPassword.current.value = '';
+    newPassword.current.value = '';
+
+    setValidationErrors([]);
+  };
+
+  const Validation = ({ type }) => {
+    if (validationErrors.length > 0) {
+      return (
+        <ul className="settings-validation">
+          {validationErrors.map((error) =>
+            error.param === type ? (
+              error.param === 'newPassword' || error.param === 'newUsername' ? (
+                error.msg
+                  .split(',')
+                  .map((err) => <li key={1 + Math.random()}>{'● ' + err}</li>)
+              ) : (
+                <li key={1 + Math.random()}>{'● ' + error.msg}</li>
+              )
+            ) : null
+          )}
+        </ul>
+      );
+    }
+
+    return null;
+  };
+
+  const handleDelete = async () => {
+    try {
+      await deleteAccount(user, dispatch);
+    } catch (err) {
+      history.push('/oops');
+    }
+  };
+
+  return (
+    <div className="settings">
+      <form onSubmit={handleSubmit}>
+        {isUpdateSuccessful ? (
+          <span>Account details updated successfully.</span>
+        ) : null}
+        <legend>USERNAME</legend>
+        <input
+          type="text"
+          id="username"
+          autoComplete="off"
+          defaultValue={user?.username}
+          ref={newUsername}
+        ></input>
+        <Validation type="newUsername" />
+        <legend>EMAIL</legend>
+        <input
+          type="text"
+          id="email"
+          autoComplete="off"
+          defaultValue={user?.email}
+          ref={newEmail}
+        ></input>
+        <Validation type="newEmail" />
+        <legend>PASSWORD</legend>
+        <label htmlFor="old-password">Current Password</label>
+        <input
+          type="password"
+          id="old-password"
+          autoComplete="off"
+          ref={currentPassword}
+        ></input>
+        <Validation type="currentPassword" />
+        <label htmlFor="new-password">New Password</label>
+        <input
+          type="password"
+          id="new-password"
+          autoComplete="off"
+          ref={newPassword}
+        ></input>
+        <Validation type="newPassword" />
+        <div className="settings-buttons">
+          <button
+            type="submit"
+            className={user?.username === 'Guest' ? 'disabled' : ''}
+            disabled={user?.username === 'Guest'}
+          >
+            Save Changes
+          </button>
+          <button type="button" onClick={cancelChanges}>
+            Cancel
+          </button>
+        </div>
+        <button
+          type="button"
+          className={user?.username === 'Guest' ? 'disabled' : 'delete-account'}
+          onClick={() => setShowConfirmation(true)}
+          disabled={user.username === 'Guest'}
+        >
+          Delete Account
+        </button>
+        {showConfirmation ? (
+          <div className="confirmation-screen">
+            <p>
+              Your account will be permenantly terminated, are you sure you want
+              to proceed?
+            </p>
+            <button type="button" onClick={() => setShowConfirmation(false)}>
+              Cancel
+            </button>
+            <Link to="/signin" style={{ textDecoration: 'none' }}>
+              <button
+                type="button"
+                className="delete-account"
+                onClick={handleDelete}
+              >
+                Delete
+              </button>
+            </Link>
+          </div>
+        ) : null}
+      </form>
+      <div className="settings-gif">
+        <img src={settingsGif} alt="Settings gif."></img>
+      </div>
+    </div>
+  );
+}
+
+export default Settings;
